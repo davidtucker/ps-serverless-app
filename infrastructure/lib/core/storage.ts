31f890a900935fdca8f73ee8b7a5d023d40b09dd
Wade Pate <wadejp8@gmail.com>
2021-10-07 11:16:02 -0600

    Bootstrapped stack, added storage.ts

---
+++ infrastructure/lib/core/storage.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+
+exposrt class AssetStorage extends cdk.Construct {
+
+    public readonly uploadBucket: s3.IBucket;
+
+    
+}
