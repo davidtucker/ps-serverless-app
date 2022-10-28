0ca9e3230776a8b0e45379090d1ccf693512d836
KeatonSmith <keatonsmith510@gmail.com>
2022-10-28 14:42:21 -0400

    Update index.ts

--- infrastructure/lib/pipeline/index.ts
+++ infrastructure/lib/pipeline/index.ts
@@ -1,5 +1,8 @@
 import { Stage, StageProps, Stack, StackProps, SecretValue, pipelines } from 'aws-cdk-lib';
+import { BuildSpec, LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+
 import { ApplicationStack } from '../core';
 
 class AppStage extends Stage {
@@ -13,21 +16,33 @@ export class ApplicationPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
-      crossAccountKeys: false,
-      synth: new pipelines.ShellStep('Synth', {
-        input: pipelines.CodePipelineSource.gitHub('KeatonSmith/ps-serverless-app', 'main', {
-          authentication: SecretValue.secretsManager('dms_config', {
-            jsonField: 'github_token',
-          }),
+    const source = CodePipelineSource.gitHub('KeatonSmith/ps-serverless-app', 'main', {
+      authentication: SecretValue.secretsManager('dms_config', {
+        jsonField: 'github_token',
+      }),
+    });
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      dockerEnabledForSynth: true,
+      codeBuildDefaults: {
+        buildEnvironment: {
+          buildImage: LinuxBuildImage.AMAZON_LINUX_2_3,
+        },
+        partialBuildSpec: BuildSpec.fromObject({
+          phases: {
+            install: {
+              commands: ['n 16.15.1'],
+            },
+          },
         }),
+      },
+      synth: new ShellStep('Synth', {
+        input: source,
         commands: ['yarn install --frozen-lockfile', 'cd infrastructure', 'npx cdk synth'],
         primaryOutputDirectory: 'infrastructure/cdk.out',
       }),
-      dockerEnabledForSynth: true,
     });
 
     pipeline.addStage(new AppStage(this, 'Staging'));
-    pipeline.buildPipeline();
   }
 }
